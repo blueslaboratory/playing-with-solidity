d14c9eb854348976cacb1dd3e89172e352b10a4c
blueslaboratory <myjunkandspam22@gmail.com>
2021-08-03 13:12:34 +0200

    doggos: variable lesson comments

--- solidity doggos.sol
+++ solidity doggos.sol
@@ -1,10 +1,12 @@
+pragma solidity ^0.4.20;
+
+/*** NOTES ***/
+/* Source: bitdegree */
+
 /*
 Version
 Solidity source files can contain any number of contract definitions. Each Solidity file also includes a thing called “Version Pragma”. It is used to prevent the code from being compiled with future compiler versions that might introduce incompatible changes. Most of the time, the definition looks like this:
 */
-
-pragma solidity ^0.4.20;
-
 
 /*
 Contracts
@@ -12,7 +14,28 @@ Contracts in Solidity are similar to classes in object-oriented languages. They 
 They are defined by using a contract keyword, followed by the contract name and and two brackets { } which will later enclose contract variables and functions. For example:
 */
 
+
+/*
+Solidity is a statically typed programming language, meaning that each variable must have its type specified. Examples of the main data types:
+
+Booleans
+Can only have one of the two following values: true or false. The keyword for booleans is bool.
+
+Integers
+Integers can be split into main groups: regular integers (can store both positive and negative values) and unsigned integers (can only store values that are 0 or higher)
+-> Regular integer has keywords from int8 to int256. The number signifies the maximum number of bits it can store (thus limiting the maximum value), and it can be any number between 8 and 256 as long as it incremented in steps of  (e.g., int16 is valid but int17 is not). The int keyword alone would be understood as int256.
+-> Unsigned integers follow the same logic. The only difference is the keyword which ranges from uint8 to uint256. Keyword uint can also be used instead of uint256.
+
+Addresses
+The address keyword is used to hold Ethereum addresses. If you're planning to store an Ethereum address, you will need to use the address keyword.
+
+Strings
+The string variable is used to store text information. If you need to store a variable that should contain text information use the string keyword.
+*/
+
+
 contract SpaceDoggos {
+
 
     uint maxPlanetsPerSystem = 10;
     uint minPlanetsPerSystem = 3;
